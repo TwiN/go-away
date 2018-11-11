52c1883904bf74e6f7103749ed9ecdf7497a9f54
TwinProduction <christian-ct@hotmail.com>
2018-11-11 14:17:05 -0500

    Added more tests

--- goaway_test.go
+++ goaway_test.go
@@ -15,12 +15,32 @@ func TestBadWords(t *testing.T)  {
 	}
 }
 
+func TestSentencesWithBadWords(t *testing.T)  {
+	sentences := []string{"What the fuck is your problem", "Go away, asshole!"}
+
+	for _, s := range sentences {
+		if !goaway.IsProfane(s) {
+			t.Error("Expected true, got false from sentence", s)
+		}
+	}
+}
+
 func TestSneakyBadWords(t *testing.T)  {
-	words := []string{"A$$", "4ss", "4s$", "a S s", "a $ s"}
+	words := []string{"A$$", "4ss", "4s$", "a S s", "a $ s", "@$$h073", "f    u     c k"}
 
 	for _, w := range words {
 		if !goaway.IsProfane(w) {
 			t.Error("Expected true, got false from word", w)
+		}
+	}
+}
+
+func TestSentencesWithSneakyBadWords(t *testing.T)  {
+	sentences := []string{"You smell p00p", "Go away, a$$h0l3!"}
+
+	for _, s := range sentences {
+		if !goaway.IsProfane(s) {
+			t.Error("Expected true, got false from sentence", s)
 		}
 	}
 }
@@ -31,6 +51,16 @@ func TestNormalWords(t *testing.T)  {
 	for _, w := range words {
 		if goaway.IsProfane(w) {
 			t.Error("Expected false, got false from word", w)
+		}
+	}
+}
+
+func TestSentencesWithNoProfanities(t *testing.T)  {
+	sentences := []string{"hello, my friend", "what's up?", "do you want to play bingo?"}
+
+	for _, s := range sentences {
+		if goaway.IsProfane(s) {
+			t.Error("Expected false, got false from sentence", s)
 		}
 	}
 }
