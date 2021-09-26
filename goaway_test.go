39ccb870366f6e0225f6b21509a3d38583e1bc26
TwinProduction <twin@twinnation.org>
2021-09-26 11:36:24 -0400

    Make sure to test the defaultProfanityDetector

--- goaway_test.go
+++ goaway_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 func TestExtractProfanity(t *testing.T) {
+	defaultProfanityDetector = nil
 	tests := []struct {
 		input             string
 		expectedProfanity string
@@ -347,6 +348,7 @@ func TestSentencesWithFalsePositivesAndProfanities(t *testing.T) {
 // "The Adventures of Sherlock Holmes" by Arthur Conan Doyle is in the public domain,
 // which makes it a perfect source to use as reference.
 func TestSentencesFromTheAdventuresOfSherlockHolmes(t *testing.T) {
+	defaultProfanityDetector = nil
 	sentences := []string{
 		"I had called upon my friend, Mr. Sherlock Holmes, one day in the autumn of last year and found him in deep conversation with a very stout, florid-faced, elderly gentleman with fiery red hair.",
 		"With an apology for my intrusion, I was about to withdraw when Holmes pulled me abruptly into the room and closed the door behind me.",
