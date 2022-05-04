aa7a16fdf9b0112817268e9217b8b7c9c129390e
Magnus Kraft <magnus.kraft@cybercom.com>
2022-05-04 21:54:24 +0200

    Fix bug when removing false positives

--- goaway.go
+++ goaway.go
@@ -174,9 +174,8 @@ func (g *ProfanityDetector) Censor(s string) string {
 		currentIndex := 0
 		for currentIndex != -1 {
 			if foundIndex := strings.Index(s[currentIndex:], word); foundIndex != -1 {
-				runeIndex := g.indexToRune(s, currentIndex+foundIndex)
 				foundRuneIndex := g.indexToRune(s, foundIndex)
-				originalIndexes = append(originalIndexes[:runeIndex], originalIndexes[foundRuneIndex+len(word):]...)
+				originalIndexes = append(originalIndexes[:foundRuneIndex], originalIndexes[foundRuneIndex+len(word):]...)
 				currentIndex += foundIndex + len([]rune(word))
 			} else {
 				break
